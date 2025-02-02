6d48917b7ae54f06d54ce1fe33e8f3fabc7c9937
Kyungmin Han <kyungmin.hahn@gmail.com>
2025-02-02 10:58:27 +0900

    data collection with AE runs ok in Prometheus

---
+++ run_script/data_collector/test_collector.py
@@ -0,0 +1,86 @@
+#! /usr/bin/env python
+import nav_msgs.msg
+import rospy
+import sys
+import os
+import yaml
+import random
+import time
+import numpy as np
+import tf2_ros
+import tf
+import datetime
+import shutil
+from std_srvs.srv import Empty
+from std_msgs.msg import Bool
+import roslaunch
+
+
+def main(argv):
+
+    base_dir = os.path.dirname(os.path.abspath(os.path.dirname(os.path.abspath(os.path.dirname(__file__)))))
+    #    config_file = '/home/hankm/catkin_ws/src/navdata_collector/param/navdata_collector.yaml'
+    config_file = '%s/param/navdata_collector.yaml' % base_dir
+
+    with open(config_file, "r") as f:
+        config = yaml.safe_load(f)
+
+    bagfile_root_path = config['navdata_collector']['bagfile_root_path']
+    now = datetime.datetime.now()
+    bagfile_path = '%s/%04d-%02d-%02d-%02d-%02d'%(bagfile_root_path,now.year, now.month, now.day, now.hour, now.minute)
+    if os.path.isdir(bagfile_path):
+        shutil.rmtree(bagfile_path)
+    os.mkdir(bagfile_path)
+
+    uuid = roslaunch.rlutil.get_or_generate_uuid(None, False)
+    roslaunch.configure_logging(uuid)
+
+    rospy.init_node('ae_data_collector_launcher', anonymous=True)
+
+    listener = tf.TransformListener()
+    rate = rospy.Rate(10.0)
+    start = time.time()
+    num_explorations = 10
+
+    for round_idx in range(0, num_explorations):
+        # make data dir
+
+        # launch files
+        roslaunch.configure_logging(uuid)
+        launch1 = roslaunch.parent.ROSLaunchParent(uuid, ["/home/hankm/catkin_ws/src/navdata_collector/launch/includes/move_former_slam.launch"])
+        launch2 = roslaunch.parent.ROSLaunchParent(uuid, ["/home/hankm/catkin_ws/src/autoexplorer/launch/autoexplorer.launch"])
+
+        cli_arg3 = ['%s/launch/includes/start_bag_async.launch' % base_dir, 'bagfile_path:=%s' % bagfile_path]
+        roslaunch_args = cli_arg3[1:]
+        roslaunch_file3 = [(roslaunch.rlutil.resolve_launch_arguments(cli_arg3)[0], roslaunch_args)]
+        launch3 = roslaunch.parent.ROSLaunchParent(uuid, roslaunch_file3)
+
+        launch1.start()
+        print(" <%d> th  move base & SLAM toolbox are up \n", round_idx)
+        time.sleep(5)
+        t = rospy.Time(0)
+        (trans, rot) = listener.lookupTransform("odom", "base_link", t)
+
+        launch2.start()
+        time.sleep(1)
+        rospy.wait_for_message('map', nav_msgs.msg.OccupancyGrid, timeout=None)
+        print("Got a map msg \n")
+
+        while not rospy.is_shutdown():
+            data = rospy.wait_for_message('exploration_is_done', Bool, timeout=None)
+            print("exploration done? %d" % data.data)
+            if data.data is True:
+                break
+        print("<%d> th exploration is done. Closing the exploration service \n")
+
+        launch3.shutdown()
+        launch2.shutdown()
+        launch1.shutdown()
+        #time.sleep(5)
+
+    print("The data collection process is completed \n")
+    end = time.time()
+    print("Total data collection time ",  (end - start) )
+
+if __name__ == '__main__':
+    main(sys.argv)
